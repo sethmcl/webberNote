2d4014ece47bbf5e54e48526ae18fc0f85ff7302
Steven Foote <smfoote@gmail.com>
2014-08-15 15:37:51 -0700

    Add the menu item and make it do something

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,9 +1,21 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function (details) {
-    console.log('previousVersion', details.previousVersion);
 });
 
-chrome.browserAction.setBadgeText({text: '\'Allo'});
+chrome.contextMenus.create({
+  title: 'Make a note',
+  type: 'normal',
+  contexts: ['selection', 'image'],
+  onclick: handleMenuClick
+});
 
-console.log('\'Allo \'Allo! Event Page for Browser Action');
+function handleMenuClick(info, tab) {
+  console.log('info:');
+  console.log(info);
+  console.log('tab:');
+  console.log(tab);
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {type: 'make-note-menu-clicked'});
+  });
+}
--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,3 +1,15 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Content script');
+function getCurrentSelection() {
+  return window.getSelection();
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender) {
+  var range, sel;
+  if (request.type === 'make-note-menu-clicked') {
+    sel = getCurrentSelection();
+    range = sel.getRangeAt(0);
+    range.surroundContents(document.createElement('mark'));
+    console.log(range.toString());
+  }
+});
